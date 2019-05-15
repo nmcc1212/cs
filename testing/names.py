664bf110221ffa8fbca48df0efe988b887861af4
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2019-05-15 19:44:14 +0100

    Add files via upload

---
+++ testing/names.py
@@ -0,0 +1,9 @@
+file = open("studentnames.txt","r")
+lines=file.readlines()
+for x in range (2):
+    for i in range (2):
+        student(i) = lines[x]
+
+
+for a in range (2):
+    print student(a)
